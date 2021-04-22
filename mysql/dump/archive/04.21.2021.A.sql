0a50fc465aabd43f9f00986c5e9a7d9300d2e658
Maxwell Kapral <kapralmaxwell@gmail.com>
2021-04-21 19:14:22 -0700

    see description
    Library_ID is now a string type.
    
    New trigger to update book quantity.
    
    Corresponding table to allow for the new trigger.
    
    ISBN is now CHAR(17)
    
    renewal is now an int instead of boolean.
    
    more tables have primary keys (may be incorrect implementation, subject to change)

--- mysql/dump/archive/04.21.2021.A.sql
+++ mysql/dump/archive/04.21.2021.A.sql
@@ -25,14 +25,15 @@ DROP TABLE IF EXISTS `Book_Copy`;
 CREATE TABLE `Book_Copy` (
   `Title` varchar(32) NOT NULL,
   `Author` varchar(32) NOT NULL,
+  `Quantity` tinyint unsigned NOT NULL,
   `Genre` varchar(32) DEFAULT NULL,
   `Availability` tinyint(1) NOT NULL,
   `Page_No` mediumint unsigned NOT NULL,
   `Edition` tinyint unsigned DEFAULT NULL,
   `Condition` varchar(32) NOT NULL,
-  `Renewal` tinyint unsigned NOT NULL,
+  `Renewal` tinyint(1) NOT NULL,
   `Copy_num` tinyint unsigned NOT NULL,
-  `ISBN` char(17) NOT NULL,
+  `ISBN` varchar(10) NOT NULL,
   PRIMARY KEY (`ISBN`,`Copy_num`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -45,57 +46,6 @@ LOCK TABLES `Book_Copy` WRITE;
 /*!40000 ALTER TABLE `Book_Copy` DISABLE KEYS */;
 /*!40000 ALTER TABLE `Book_Copy` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-/*!50032 DROP TRIGGER IF EXISTS update_total_books */;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`ubuntu`@`localhost`*/ /*!50003 TRIGGER `update_total_books` AFTER INSERT ON `Book_Copy` FOR EACH ROW BEGIN
-    SET @COUNT=(SELECT COUNT(*) FROM Book_Quantity WHERE (ISBN=NEW.ISBN));
-    IF @COUNT=0 THEN
-        INSERT INTO Book_Quantity VALUES(NEW.ISBN, 1);
-    ELSE
-        UPDATE Book_Quantity
-        SET
-            Quantity = Quantity + 1
-        WHERE ISBN = NEW.ISBN;
-    END IF;
-END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-
---
--- Table structure for table `Book_Quantity`
---
-
-DROP TABLE IF EXISTS `Book_Quantity`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Book_Quantity` (
-  `ISBN` char(17) NOT NULL,
-  `Quantity` tinyint unsigned NOT NULL,
-  PRIMARY KEY (`ISBN`),
-  CONSTRAINT `Book_Quantity_ibfk_1` FOREIGN KEY (`ISBN`) REFERENCES `Book_Copy` (`ISBN`),
-  CONSTRAINT `Book_Quantity_chk_1` CHECK ((`Quantity` > 0))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Book_Quantity`
---
-
-LOCK TABLES `Book_Quantity` WRITE;
-/*!40000 ALTER TABLE `Book_Quantity` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Book_Quantity` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `Check_Out`
@@ -105,11 +55,11 @@ DROP TABLE IF EXISTS `Check_Out`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Check_Out` (
-  `Library_ID` char(10) NOT NULL,
+  `Library_ID` decimal(10,0) NOT NULL,
   `Due_date` date NOT NULL,
-  `ISBN` char(17) NOT NULL,
+  `ISBN` varchar(10) NOT NULL,
   `Copy_num` tinyint unsigned NOT NULL,
-  PRIMARY KEY (`Library_ID`,`ISBN`,`Copy_num`),
+  KEY `Library_ID` (`Library_ID`),
   KEY `ISBN` (`ISBN`,`Copy_num`),
   CONSTRAINT `Check_Out_ibfk_1` FOREIGN KEY (`Library_ID`) REFERENCES `Member` (`Library_ID`),
   CONSTRAINT `Check_Out_ibfk_2` FOREIGN KEY (`ISBN`, `Copy_num`) REFERENCES `Book_Copy` (`ISBN`, `Copy_num`)
@@ -157,7 +107,7 @@ DROP TABLE IF EXISTS `Member`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Member` (
   `Name` varchar(32) NOT NULL,
-  `Library_ID` char(10) NOT NULL,
+  `Library_ID` decimal(10,0) NOT NULL,
   `Street` varchar(32) DEFAULT NULL,
   `Zip_Code` decimal(6,0) DEFAULT NULL,
   `City` varchar(32) DEFAULT NULL,
@@ -187,10 +137,10 @@ DROP TABLE IF EXISTS `Overdue_Books`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Overdue_Books` (
-  `Library_ID` char(10) NOT NULL,
-  `ISBN` char(17) NOT NULL,
+  `Library_ID` decimal(10,0) NOT NULL,
+  `ISBN` varchar(10) NOT NULL,
   `Copy_num` tinyint unsigned NOT NULL,
-  PRIMARY KEY (`Library_ID`,`ISBN`,`Copy_num`),
+  KEY `Library_ID` (`Library_ID`),
   KEY `ISBN` (`ISBN`,`Copy_num`),
   CONSTRAINT `Overdue_Books_ibfk_1` FOREIGN KEY (`Library_ID`) REFERENCES `Member` (`Library_ID`),
   CONSTRAINT `Overdue_Books_ibfk_2` FOREIGN KEY (`ISBN`, `Copy_num`) REFERENCES `Book_Copy` (`ISBN`, `Copy_num`)
@@ -214,10 +164,10 @@ DROP TABLE IF EXISTS `Preferences`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Preferences` (
-  `Library_ID` char(10) NOT NULL,
+  `Library_ID` decimal(10,0) NOT NULL,
   `type` varchar(32) DEFAULT NULL,
   `value` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`Library_ID`),
+  KEY `Library_ID` (`Library_ID`),
   CONSTRAINT `Preferences_ibfk_1` FOREIGN KEY (`Library_ID`) REFERENCES `Member` (`Library_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -240,4 +190,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-21 19:00:19
+-- Dump completed on 2021-04-21 12:54:12
--- mysql/pretty/latest/triggers.md
+++ mysql/pretty/latest/triggers.md
@@ -11,4 +11,20 @@ BEGIN
         Copy_num = NEW.Copy_num;
 END;
 ```
+```mysql
+-- update quantity after insert of book copy
+CREATE TRIGGER update_total_books AFTER INSERT
+    ON Book_Copy FOR EACH ROW
+BEGIN
+    SET @COUNT=(SELECT COUNT(*) FROM Book_Quantity WHERE (ISBN=NEW.ISBN));
+    IF @COUNT=0 THEN
+        INSERT INTO Book_Quantity VALUES(NEW.ISBN, 1);
+    ELSE
+        UPDATE Book_Quantity
+        SET
+            Quantity = Quantity + 1
+        WHERE ISBN = NEW.ISBN;
+    END IF;
+END;
+```
 [May need to add DELIMITERS](https://www.mysqltutorial.org/mysql-triggers/mysql-after-insert-trigger/)
